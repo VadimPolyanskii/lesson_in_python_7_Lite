d6e24de9a1169330cc99831b91c9f7aaef0cba3f
Vad Pol <work.arb19@gmail.com>
2021-12-20 14:50:45 +0300

    Data to json

--- json_example.py
+++ json_example.py
@@ -1,9 +1,1 @@
-# Создадим файл json
-
-# Подключаем модуль json
-import json
-
-dict_avto = {'brand': 'Volvo', 'Model': 'S60', 'Year': '2021', 'Price (million) ': '3.0'}
-
-with open('data_avto.json', 'w') as f:
-    json.dump(dict_avto, f)
+# Создадим файл json# Подключаем модуль jsonimport jsondict_avto = {'brand': 'Volvo', 'Model': 'S60', 'Year': '2021', 'Price (million) ': '3.0'}with open('data_avto.json', 'w') as f:    json.dump(dict_avto, f)
